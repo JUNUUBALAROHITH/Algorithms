73565895378f2bf854deeb53dab80d51210b0631
JUNUUBALAROHITH <102303532+JUNUUBALAROHITH@users.noreply.github.com>
2023-03-25 22:58:01 +0530

    Add files via upload

---
+++ dynamic programming.cpp
@@ -0,0 +1,400 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const int MAX = 100001;
+
+// DP --> recursion + memorisation(OVERLAPPING SUBPROBLEMS)
+// 1) Combinations and Probabilty 
+// 2) Optimisation(Minimum/ Maximum)
+// 3) YES/NO problems
+
+        // 1
+// Fibonacci Optimiation using dynamic programming
+int dp[MAX]; // O(n)
+
+int fib(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    if (dp[n] != -1)
+        return dp[n];
+    return dp[n] = fib(n - 1) + fib(n - 2); // memorization and recursion
+}
+int fib(int n)
+{
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+}                                           // iterative approach
+
+        // 2
+// Catalan number Cn=sigma(i =0 to n-1)(Ci * Cn-i-1)
+long long catalan(int n){
+    if(n<=1){
+        return 1;
+    }
+    long long ans=0;
+    for(int i=0;i<n;i++) ans+=catalan(i)*catalan(n-i-1);
+    return ans;
+}
+
+        // 3
+// Bell numbers no of ways of partitioning a set of n numbers
+// S(n,k)--> no of partitions of n elements into k sets
+// bell(n)--> sigma(k=1 to n)S(n,k)
+//  S(n+1,k)=k*S(n,k)+S(n,k-1)
+// 1) It is added as a single element set to existing partitions, i.e, S(n, k-1) 
+// 2) It is added to all sets of every partition, i.e., k*S(n, k)
+
+        // 4
+// Binomial Coefficient recursion C(n, k) = C(n-1, k-1) + C(n-1, k) 
+int binomialCoeffUtil(int n, int k, int** dp)
+{
+    // If value in lookup table then return
+    if (dp[n][k] != -1) //    
+        return dp[n][k];
+ 
+    // store value in a table before return
+    if (k == 0) {
+        dp[n][k] = 1;
+        return dp[n][k];
+    }
+     
+    // store value in table before return
+    if (k == n) {
+        dp[n][k] = 1;
+        return dp[n][k];
+    }
+     
+    // save value in lookup table before return
+    dp[n][k] = binomialCoeffUtil(n - 1, k - 1, dp) +
+               binomialCoeffUtil(n - 1, k, dp);
+    return dp[n][k];
+}
+ 
+int binomialCoeff(int n, int k)
+{
+    int** dp; // make a temporary lookup table
+    dp = new int*[n + 1];
+ 
+    // loop to create table dynamically
+    for (int i = 0; i < (n + 1); i++) {
+        dp[i] = new int[k + 1];
+    }
+ 
+    // nested loop to initialise the table with -1
+    for (int i = 0; i < (n + 1); i++) {
+        for (int j = 0; j < (k + 1); j++) {
+            dp[i][j] = -1;
+        }
+    }
+ 
+    return binomialCoeffUtil(n, k, dp);
+}
+
+        // 5
+// COIN CHANGE min no of coins required for given amount
+int dp[10010];
+int fun(vector<int> &coins,int amount){
+    if(amount==0) return 0;
+    if(dp[amount]!=-1) return dp[amount];
+    int ans=INT_MAX;
+    for(int i=0;i<coins.size();i++){
+        if(amount-coins[i]>=0) ans=min(ans+0LL,fun(coins,amount-coins[i])+1LL);
+    }
+    return dp[amount]=ans;
+}
+int coinChange(vector<int>& coins, int amount) {
+    memset(dp,-1,sizeof(dp));
+    if(fun(coins,amount)==INT_MAX) return -1;
+    return fun(coins,amount);
+}
+
+        // 6
+// subset sum 
+// n=6 {3,34,4,12,5,2}
+// sum=30
+// op: false
+bool f(int i,int sum,vector<int> &v,vector<vector<int>> &dp){
+    if(i==0) {
+        if(sum==0 || sum==v[0]) return 1;
+        return 0;
+    }
+    if(sum==0) return 1;
+    if(dp[i][sum]!=-1) return dp[i][sum];
+    if(v[i]<=sum) return dp[i][sum]=f(i-1,sum-v[i],v,dp)||f(i-1,sum,v,dp);
+    return dp[i][sum]=f(i-1,sum,v,dp);
+}
+
+bool isSubsetSum(vector<int>v, int sum){
+    // code here 
+    int n=v.size();
+    vector<vector<int>> dp(n,vector<int> (sum+1,-1));
+    return f(n-1,sum,v,dp);
+}
+
+        // 7
+// Rod cutting 
+// n=8 {1,5,8,9,10,17,17,20}
+// op: 22(5+17)
+int cutRod(int price[], int n) {
+    //code here
+    int dp[n+1][n+1];
+    for(int i=0;i<n+1;i++) dp[0][i]=0;
+    for(int i=0;i<n+1;i++) dp[i][0]=0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i<=j){
+                dp[i][j]=max(price[i-1]+dp[i][j-i],dp[i-1][j]);
+            }
+            else dp[i][j]=dp[i-1][j];
+        }
+    }
+    return dp[n][n];
+}
+
+        // 8
+// painting ways
+// n walls k colors no three consecutive are same
+int mod=1e9+7;
+long long f(int n,int k,vector<long long> &dp){
+    if(n==0) return k;
+    else if(n==1) return k*k;
+    if(dp[n]!=-1) return dp[n];
+    return dp[n]=((k-1)*(f(n-1,k,dp)%mod+f(n-2,k,dp)%mod))%mod;
+}
+
+long long countWays(int n, int k){
+    vector<long long> dp(n+1,-1);
+    return f(n-1,k,dp);
+}
+
+        // 9
+// longest increasing subsequence
+int f(int i,int j,int a[],vector<vector<int>> &dp,int n){
+    if(i==n) return 0;
+    if(dp[i][j+1]!=-1) return dp[i][j+1];
+    if(j==-1 || a[i]>a[j]){
+        return dp[i][j+1]=max(1+f(i+1,i,a,dp,n),f(i+1,j,a,dp,n));
+    }
+    return dp[i][j+1]=f(i+1,j,a,dp,n);
+}
+
+int longestSubsequence(int n, int a[])
+{
+    vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
+    return f(0,-1,a,dp,n);
+}
+
+        // 10
+// max square submatrix having all elements 1s 
+int f(int n,int m,vector<vector<int>> &v,vector<vector<int>> &dp){
+    if(n<0 || m<0) return 0;
+    if(dp[n][m]!=-1) return dp[n][m];
+    if(v[n][m]==1) return dp[n][m]=1+min(f(n-1,m-1,v,dp),min(f(n-1,m,v,dp),f(n,m-1,v,dp)));
+    int x=f(n-1,m-1,v,dp),y=f(n-1,m,v,dp),z=f(n,m-1,v,dp);
+    return dp[n][m]=0;
+}
+
+int maxSquare(int n, int m, vector<vector<int>> v){
+    vector<vector<int>> dp(n,vector<int> (m,-1));
+    for(int i=0;i<n;i++) dp[i][0]=v[i][0];
+    for(int i=0;i<m;i++) dp[0][i]=v[0][i];
+    int k= f(n-1,m-1,v,dp);
+    int c=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            // cout<<dp[i][j]<<" ";
+            if(c<dp[i][j]) c=dp[i][j];
+        }
+        // cout<<'\n';
+    }
+    return c;
+}
+
+        // 11
+// max path sum in matrix
+// (i,j)->(i+1,j),(i+1,j+1),(i+1,j-1) possible
+int f(int i,int j,int n,vector<vector<int>> &v,vector<vector<int>> &dp){
+    if(i==n-1) return v[i][j];
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(j==0 && j<n-1) return dp[i][j]=v[i][j]+max(f(i+1,j,n,v,dp),f(i+1,j+1,n,v,dp));
+    else if(j==0 && j==n-1) return dp[i][j]=v[i][j]+f(i+1,j,n,v,dp);
+    else if(j>0 && j==n-1) return dp[i][j]=v[i][j]+max(f(i+1,j-1,n,v,dp),f(i+1,j,n,v,dp));
+    return dp[i][j]=v[i][j]+max(f(i+1,j,n,v,dp),max(f(i+1,j+1,n,v,dp),f(i+1,j-1,n,v,dp)));
+}
+
+int maximumPath(int n, vector<vector<int>> v)
+{
+    vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+    int c=0;
+    for(int i=0;i<n;i++) c=max(c,f(0,i,n,v,dp));
+    return c;
+}
+
+        // 12
+// min no of jumps to reach end of array
+// already done by bottom up memorization
+
+        // 13
+// longest common substring
+int longestCommonSubstr (string s1, string s2, int n, int m)
+{
+    int dp[n+1][m+1];
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=m;j++){
+            if(i==0 || j==0) dp[i][j]=0;
+            else if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+            else dp[i][j]=0;
+        }
+    }
+    int c=0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            c=max(c,dp[i][j]);
+        }
+    }
+    return c;
+}
+
+// Bottom Up approach : iterative approach
+// Top Bottom approach : recursion + memorisation approach
+// 1) Vacation
+// const int MAX = 100001;
+// int points[MAX][3];
+// int dp[MAX][3];
+
+// int ans(int n, int j)
+// {
+//     if (n == 1)
+//         return max(points[n - 1][0], max(points[n - 1][1], points[n - 1][2]));
+//     if (dp[n][j] != -1)
+//         return dp[n][j];
+//     int m = 0;
+//     for (int i = 0; i < 3; i++)
+//     {
+//         if (i != j)
+//         {
+//             m = max(m, ans(n - 1, i));
+//         }
+//     }
+//     return dp[n][j] = m + points[n - 1][j];
+// }
+
+// void solve()
+// {
+//     int n;
+//     cin >> n;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> points[i][0] >> points[i][1] >> points[i][2];
+//     }
+//     cout << max(ans(n, 0), max(ans(n, 1), ans(n, 2))) << '\n';
+// }
+
+// 2) dp[i]=>maximuim score you can get by considering numbers till i;
+
+
+
+// whether subset exists with target sum
+// memorization
+bool f(int ind,int k,vector<int> &v,vector<vector<int>> &dp,int n){
+    if(k==0) return true;
+    if(ind<0) return false;
+    if(dp[ind][k]!=-1) return dp[ind][k];
+    if(v[ind]<=k){
+        return dp[ind][k]=(f(ind-1,k,v,dp,n)|(f(ind-1,k-v[ind],v,dp,n)));
+    }
+    return dp[ind][k]=f(ind-1,k,v,dp,n);
+}
+
+bool subsetSumToK(int n, int k, vector<int> &v) {
+    // Write your code here.
+    vector<vector<int>> dp(n+1,vector<int> (k+1,-1));
+    return f(n-1,k,v,dp,n);
+}
+// tabulation
+bool subsetSumToK(int n, int k, vector<int> &v) {
+    bool dp[n+1][k+1];
+    for(int i=0;i<=k;i++) dp[0][i]=0;
+    for(int i=0;i<=n;i++) dp[i][0]=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=k;j++){
+            int take=0;
+            if(v[i-1]<=j) dp[i-1][j-v[i]];
+            int n=dp[i-1][j];
+            dp[i][j]=take|n;
+        }
+    }
+    return dp[n][k];
+}
+
+// no of subsets with sum k
+int f(int ind,int sum,vector<int> &v,vector<vector<int>> &dp){
+    if(ind==0){
+        if(sum==0 && v[0]==0) return 2;
+        if(sum==0 || v[0]==sum) return 1;
+        return 0;
+    }
+    if(dp[ind][sum]!=-1) return dp[ind][sum];
+    if(v[ind]<=sum) return dp[ind][sum]=f(ind-1,sum-v[ind],v,dp)+f(ind-1,sum,v,dp);
+    return dp[ind][sum]=f(ind-1,sum,v,dp);
+}
+
+int findWays(vector<int> &v, int k)
+{
+    // Write your code here.
+    int n=v.size();
+    vector<vector<int>> dp(n,vector<int> (k+1,-1));
+    return f(n-1,k,v,dp);
+}
+
+// two subsets with min abs diff
+int minSubsetSumDifference(vector<int>& v, int n)
+{
+	// Write your code here.
+	int k=0;
+	for(auto &i:v) k+=i;
+	bool dp[n+1][k+1];
+	for(int i=0;i<=k;i++) dp[0][i]=0;
+	for(int i=0;i<=n;i++) dp[i][0]=1;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=k;j++){
+			int take=0;
+			if(v[i-1]<=j) take=dp[i-1][j-v[i-1]];
+			int n=dp[i-1][j];
+			dp[i][j]=take|n; 
+		}
+	}
+	int m=INT_MAX;
+	for(int i=0;i<=k;i++){
+		if(dp[n][i]==1) m=min(m,abs(i-(k-i)));
+	}
+	return m;
+}
+
+int t;
+void solve()
+{
+}
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    for (int i = 0; i < MAX; i++)
+        dp[i] = -1;
+    // cin >> t;
+    t = 1;
+    while (t--)
+    {
+        cout << fib(5);
+        solve();
+    }
+}
---
+++ graphs adv.cpp
@@ -0,0 +1,391 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int t;
+const int MAX = 1e9 + 7;
+
+// declaration of priority queue with ascending order 
+priority_queue<int, vector<int>, greater<int>> q;
+
+
+// null graph --> no edges between the vertices
+// trivia graph --> having only one vertex
+// degree of self loop=2
+// degree of node in directed graph = indegree+outdegree
+
+vector< vector< int> > v(n);
+// adj[1]-->vector<int>
+// adj[1].push_back(2)
+
+// Check if 0-4 forms an edge or not?
+for(int i=0;i<v[0].size();i++)
+{
+    if(v[0][i]==4)
+    return true;
+}
+
+// breath first search
+int level[n];
+vector<int> bfsOfGraph(int V, vector<int> adj[]){
+    vector<bool> visited(V, false);
+    int s = 0;
+    visited[s] = true; 
+    vector<int> res;
+    queue<int> q;
+    q.push(s);
+    level[s]=0;
+    while (!q.empty()) {
+        int t = q.front();
+        res.push_back(t);
+        q.pop();
+        for(int v : adj[t])
+            if(!visited[v]){ 
+                visited[v] = true;
+                level[v]=level[t]+1;  //stores distance from src node
+                q.push(v);
+            }
+    }
+    return res;
+}
+
+// If there are multiple sources, we will put the all source
+// vertices to the queue at first rather than a single
+// vertex which was in case of standard BFS.
+//  connected graph with n nodes and n-1 edges is a tree
+// connected graph with n node and n edges is a graph with exactly one cycle
+// each node has single parent in tree
+
+bool vis[n+1];
+
+void dfs(int v,int adj[n+1][]){  // O(V+E)
+    vis[v]=1;
+    cout<<v<<"->";
+    for(int child : adj[v]){
+        if(vis[child]==0) dfs(child);
+    }
+}
+
+int cc(int adj[n+1][]){
+    int c=0;
+    for(int i=1;i<=n;i++){
+        if(vis[i]==0){
+            c++;
+            dfs(i);
+        }
+    }
+    return c;
+}
+
+// Bipartite graph
+// each vertex belongs to exactly one of two disjoin sets
+// each edge connects vertices of 2 different sets
+
+bool bipartite(int v,int c,vector<vector<int>> &adj,vector<bool> &col){
+    vis[v]=1;
+    col[v]=c;
+    for(int child:adj[v]){
+        if(!vis[v]){
+            if(bipartite(child,c^1)==false) return false;
+        }
+        else{
+            if(col[v]==col[child]) return false;
+        }
+    }
+    return true;
+}
+
+// if connected components=1 && e=v-1 --> tree topology
+
+// cycle detection using dfs
+bool cycleDetection(int node,int par){
+    vis[node]=1;
+    for(int child:adj[node]){
+        if(vis[child]==0){
+            if(cycleDetection(child,node)) return 1;
+        }
+        else{
+            if(child!=par) return 1;
+        }
+    }
+    return 0;
+}
+
+// in /out time implementation in dfs
+int timer=1;
+bool dfs(int v){
+    vis[v]=1;
+    in[v]=timer++;
+    for(int child:v){
+        if(!vis[j]){
+            dfs(child);
+        }
+    }
+    out[v]=timer++;
+}
+
+// diameter of tree 
+// naive approch : O(n^2)
+// better approch:
+// only 2 dfs runs
+// take arbitary node as root ,run dfs find farthest node let it be x
+// run dfs from x and find max distance from this node to any other node this distance is diameter
+// implementation:
+int ma = -1, maxNode;
+
+void dfs(int v, vector<vector<int>> &adj, vector<bool> &vis, int dis)
+{
+    vis[v] = 1;
+    if (ma < dis)
+    {
+        ma = dis;
+        maxNode = v;
+    }
+    for (auto i : adj[v])
+    {
+        if (!vis[i])
+        {
+            dfs(i, adj, vis, dis + 1);
+        }
+    }
+}
+
+// BRIDGE: A Bridge is an edge which when removed increases connected components
+// Back edges can never be bridges
+int timer;
+// DFS based implementation to find bridge edges
+
+void dfs(int node ,int par){
+    vis[node]=1;
+    in[node]=low[node]=timer;
+    timer++;
+    for(auto child:adj[node]){
+        if(child==par) continue;
+        if(vis[child]){
+            // back edge
+            low[node]=min(low[node],in[child]);
+        }
+        else{
+            // forward edge
+            dfs(child,node);
+            if(low[child]>in[node]){
+                cout<<node<<" "<<child<<" is a bridge\n";
+                low[node]=min(low[node],low[child]);
+            }
+        }
+    }
+}
+
+// ARTICULATION POINT: vertex which when removed increases the no of connected components
+// there exist articulation point without bridges
+// end points of a bridge will be articulation point if that node has degree atleast 2
+
+int n; // number of nodes
+vector<vector<int>> adj; // adjacency list of graph
+
+vector<bool> visited;
+vector<int> tin, low;
+int timer;
+
+void dfs(int v, int p = -1) {
+    visited[v] = true;
+    tin[v] = low[v] = timer++;
+    int children=0;
+    for (int to : adj[v]) {
+        if (to == p) continue;
+        if (visited[to]) {
+            low[v] = min(low[v], tin[to]);
+        } else {
+            dfs(to, v);
+            low[v] = min(low[v], low[to]);
+            if (low[to] >= tin[v] && p!=-1)
+                IS_CUTPOINT(v);
+            ++children;
+        }
+    }
+    if(p == -1 && children > 1)
+        IS_CUTPOINT(v);
+}
+
+void find_cutpoints() {
+    timer = 0;
+    visited.assign(n, false);
+    tin.assign(n, -1);
+    low.assign(n, -1);
+    for (int i = 0; i < n; ++i) {
+        if (!visited[i])
+            dfs (i);
+    }
+}
+
+// Topological sort is not unique for same graph
+// many valid are possible
+// If there exist cycle in the graph then there is no topological sort exists
+// Kahn`s algo:choose each time a vertex such that its indegree is zero and remove all its edges and continue same step
+int in[v+1]; // to store in degree of each vertex
+// for(int i=1;i<=e;i++){
+//     cin>>x>>y;
+//     adj[x].push_back(y);
+//     in[y]++; // directed graph
+// }
+vector<int> TopoSort(vector<vector<int>> &adj,int v){
+    queue<int> q;
+    vector<int> res; // to store result
+    for(int i=1;i<=n;i++){
+        if(in[i]==0) q.push(i);
+    }
+    while(!q.empty()){
+        int cur=q.front();
+        res.push_back(cur);
+        q.pop();
+        for(auto child: adj[cur]){
+            in[child]--;
+            if(in[child]==0) q.push(child);
+        }
+    }
+    return res;
+}
+
+void solve()
+{
+    int v,e;
+    cin >> v>>e;
+    vector<vector<int>> adj(v + 1);
+    int x, y;
+    for (int i = 0; i < e; i++)
+    {
+        cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    for(int i=0;i<=v;i++){
+        sort(adj[i].begin(),adj[i].end())
+    }
+    vector<bool> vis(n + 1, 0);
+    dfs(1, adj, vis, 0);
+    for (int i = 1; i < n + 1; i++)
+        vis[i] = 0;
+    ma = -1;
+    dfs(maxNode, adj, vis, 0);
+    cout << ma << '\n';
+}
+
+// counting subtrees of all nodes
+int dfs(int node){
+    vis[node]=1;
+    int cur_size=1;
+    for(auto child:adj[node]){
+        if(vis[child]==0) cur_size+=dfs(child);
+    }
+    subSize[node]=cur_size;
+    return cur_size;
+}
+
+// grids
+// cell-->node
+// side-->edge
+
+// Applying DFS on 2D grid
+
+bool isValid(int x,int y){
+    if(x<1 || y<1 || x>n || y>m) return false;
+    return true;
+}
+
+int dx[4]={0,0,-1,1}
+int dy[4]={1,-1,0,0};
+void dfs(int x,int y){
+    vis[x][y]=1;
+    if(isValid(x-1,y)) dfs(x-1,y);
+    if(isValid(x,y-1)) dfs(x,y-1);
+    if(isValid(x+1,y)) dfs(x+1,y);
+    if(isValid(x,y+1)) dfs(x,y+1);
+    /*...........or.............*/
+    for(int i=0;i<4;i++){
+        if(isValid(x+dx[i],y+dy[i])){
+            dfs(x+dx[i],y+dy[i]);
+        }
+    }
+}
+
+// no of connected components
+int cnt=0;
+for(int i=1;i<=n;i++){
+    for(int j=1;j<=m;j++){
+        if(v[i][j]==1 && vis[i][j]==0){
+            cnt++;
+            dfs(i,j);
+        }
+    }
+}
+
+// Applying BFS on 2D grids
+// case1:edges-->common sides only
+// case2:edges-->common sides+common corners
+
+void BFS(int srcX,int srcY){
+    queue<pair<int,int>> q;
+    q.push({srcX,srcY});
+    dist[srcX][srcY]=0;
+    vis[srcX][srcY]=1;
+    while(!q.empty()){
+        int currX=q.front().first;
+        int currY=q.front().second;
+        q.pop();
+        for(int i=0;i<4;i++){
+            if(isValid(currX+dx[i],currY+dy[i])){
+                int newX=currX+dx[i];
+                int newY=currY+dy[i];
+                dist[newX][newY]=dist[currX][currY]+1;
+                vis[newX][newY]=1;
+                q.push({newX,newY});
+            }
+        }
+    }
+}
+
+
+// Storongly Connected Components
+// for every pair of vertices there exist a path
+// Transposed Graph: reversing direction of edges in directed graph
+// Condensation graph:
+// Each SCC acts as vertex in condensation graph
+// Condensation graph doesnt contain any cycle
+// if Ci ,Cj are SCC and there exists edge from Ci to Cj then out[Ci]>out[Cj]
+
+
+void solve()
+{
+    int n>>m;
+    cin>>n>>m;
+    int adj[n+1][m+1];
+    for(int i=0;i<m;i++){  // adjacency matrix
+        int u,v;
+        cin>>u>>v;
+        adj[u][v]=1;
+        adj[v][u]=1;
+    }
+
+    for(int i=0;i<m;i++){  // adjacency list
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    } 
+}
+
+int32_t main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
